d642c53e84bb40d3a96cbd2438c3a853db900107
Aayush <aayush111.ak@gmail.com>
2023-11-27 19:49:20 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score)); // method 
+// console.log(typeof score);
+// console.log(typeof(score)); // method 
 
 // let valueInnumber = Number(score)
 // console.log(typeof valueInnumber);
@@ -18,15 +18,49 @@ console.log(valueInnumber); ----> 1
 "33abc" 
 */
 
-let isLoggedIn =1 
+// let isLoggedIn =1 
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); 
 //it gives true but if we keep isloggedIn = "" it will give false.
 //"hitesh" -->true // 0 --> false
 
-let someNumber = 42
+// let someNumber = 42
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************** operations ****************************************//
+
+let value = 3
+let negvalue = -value;
+//console.log(negvalue); --> -3
+
+let str1 = "hello"
+let str2 = " aayush"
+
+let str3= str1 + str2
+//console.log(str3);
+
+//------refer to toprimitive 7.1.1 guidelines of ecma in tc39.es------------------
+// console.log("1"+2); -->12
+// console.log(1 + "2");---> 12
+// console.log("1" + 2 + 2); --> 122
+
+// console.log(1 + 2 + '2'); --> 32
+// console.log("3"+'23');--> 323
+
+// console.log(true);-->true
+// console.log(+true);-->1
+// console.log(+"");-->0
+// console.log(true+); --->error
+
+let num1,num2,num3
+
+num1=num2=num3 = 2+2
+
+let gameCounter =100
+ // gameCounter++; --->101 postfix see prefix and postfix js mdn
+ // ++gameCounter; --->101 prefix
+console.log(gameCounter);
