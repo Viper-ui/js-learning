d882920fd8651ece0de0769bb11ba8bbd08bc15e
Aayush <aayush111.ak@gmail.com>
2023-11-27 19:08:09 +0000

    variables + datatypes with notes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail= "aayush@google.com"
+var accountpassword ="12345"
+accountCity ="jaipur"
+
+let accountState;
+
+//accountId=2
+
+
+accountEmail="hc@gc.com"
+accountpassword="212112"
+accountCity="kolkata"
+
+// console.log(accountId); not allowed
+/*
+Prefer not to use var 
+Because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  error because we are using nodejs not browser and browser contains js engines so it will be executed there 
+
+console.log(3 +
+    3); //not proper coding ...code redability should be high
+
+  //  tc39.es & mdn // for js docs
+
+    let name ="aayush";
+    let age =18
+    let isLoggedIn = false
+
+    /*
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value // representation of empty value
+    undefined => value not assigned
+    symbol => unique
+
+
+    object
+    */
+
+    console.log(typeof age);
+
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
+   
