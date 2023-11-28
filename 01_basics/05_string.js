e79ec35ee3190a5c9d3689d9bdcbdd73b7a93ffd
Aayush <aayush111.ak@gmail.com>
2023-11-28 09:38:18 +0000

    string starting

---
+++ 01_basics/05_string.js
@@ -0,0 +1,16 @@
+const name ="aayush"
+
+const repoCount = 50
+
+console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String('aayush123') 
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);// get prototype object of console
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
